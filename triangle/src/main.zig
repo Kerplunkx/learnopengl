60e2932a4f1f01bef86b3dd35ea0db6a0f860173
Guillermo Moreira <guillermomoreira01@gmail.com>
2023-09-27 09:39:22 -0500

    first commit

---
+++ triangle/src/main.zig
@@ -0,0 +1,44 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("zgl");
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    const window = glfw.Window.create(800, 600, "Hello, mach-glfw!", null, null, .{
+        .opengl_profile = .opengl_core_profile,
+        .context_version_major = 4,
+        .context_version_minor = 5,
+    }) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.loadExtensions(proc, glGetProcAddress);
+    gl.viewport(0, 0, 800, 600);
+
+    while (!window.shouldClose()) {
+        gl.clearColor(0.8, 0.8, 0.8, 1.0);
+        gl.clear(.{ .color = true });
+
+        window.swapBuffers();
+        glfw.pollEvents();
+    }
+}
+
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.binding.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
